f3ede3fef232e5d774c7f4af0bb7402d88011403
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-04-21 17:06:39 +0300

    add about tab

--- src/popup/Root.vue
+++ src/popup/Root.vue
@@ -3,14 +3,14 @@
         <header>
             <span><strong>Translate and Study with Beemo</strong>!</span>
             <span>
-                <a href="#" @click="openInstructions = !openInstructions"><icon class="how-to-icon" name="question-circle"></icon></a>
+                <a href="#" @click="openAbout = !openAbout"><icon class="how-to-icon" name="question-circle"></icon></a>
                 <a href="#" @click="openOptionsPage"><icon name="cog"></icon></a>
             </span>
         </header>
         <div v-if="loading">
             Loading ...
         </div>
-        <div v-if="!openInstructions && !loading" class="main">
+        <div v-if="!openAbout && !loading" class="main">
             <div class="vocabulary">
                 <div v-if="!vocabulary.length" class="empty">
                     <img src="https://orig00.deviantart.net/40d9/f/2012/221/5/b/beemo_dancing_by_norrling-d5afmpo.gif"
@@ -45,8 +45,23 @@
             </div>
             <el-button size="mini" type="primary" @click="tab">Open in new tab</el-button>
         </div>
-        <div v-if="openInstructions && !loading" class="instructions">
-            instructions ...
+        <div v-if="openAbout && !loading" class="instructions">
+            <p class="muted">Meet Beemo! The most friendliest Chrome Translation Extension! ❤️</p>
+            <el-collapse v-model="activeNames">
+                <el-collapse-item title="How to use" name="1">
+                    <div>Using Beemo can't be easier! Just select text on the page, and all magic is done!</div>
+                    <div>You can change translation language
+                        and add translation to you own vocabulary. <br>
+                        To see your vocabulary click
+                        <a href="#" @click="tab" class="link">Open ine a new tab</a> and enjoy!</div>
+                </el-collapse-item>
+                <el-collapse-item title="What is next" name="2">
+                    <div>Beemo is always working on updates to improve your experience using Beemo. New features are comming!</div>
+                </el-collapse-item>
+                <el-collapse-item title="Feedback" name="3">
+                    <div>Help Beemo to become better! We're glad to receive any suggestion <span class="muted">here</span></div>
+                </el-collapse-item>
+            </el-collapse>
         </div>
     </div>
 </template>
@@ -54,9 +69,10 @@
   export default {
     data() {
       return {
-        openInstructions: false,
+        openAbout: false,
         vocabulary: [],
-        loading: false
+        loading: false,
+        activeNames: ['1']
       };
     },
     methods: {
@@ -90,13 +106,21 @@
     }
   };
 </script>
-<style scoped lang="scss">
+<style lang="scss">
     $grey: #3C4858;
     $light-grey: #8492A6;
     $super-light-grey: #dcdcdc;
 
+    .el-collapse-item__header {
+        font-weight: bold;
+    }
+
     .muted {
         color: $light-grey;
+    }
+
+    .link {
+        color: #67c23a;
     }
 
     .popup {
